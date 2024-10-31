c757f7a93747732861613200616628186df32dbe
Leverdrai <69008499+Leverdrai@users.noreply.github.com>
2024-10-31 16:32:02 +0100

    codice

---
+++ Assets/Script/GameSingleton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSingleton : MonoBehaviour
+{
+    public static GameSingleton instance;
+    public Ascia ascia;
+    public Pino pino;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        ascia = new Ascia();
+        pino = new Pino();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
