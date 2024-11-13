16f4882ebc3927afe8b8f626f469832a785f83e0
Leverdrai <69008499+Leverdrai@users.noreply.github.com>
2024-11-13 12:54:50 +0100

    tronco

---
+++ Assets/Script/tronco.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class troncoz : MonoBehaviour
+{
+    public GameObject Tronco;
+    public Ascia ascia;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
