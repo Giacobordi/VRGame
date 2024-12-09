7106ba6c87a3ad49f6544e8575b6bfe79c72e4c1
Leverdrai <ale.levesio@gmail.com>
2024-12-09 13:04:00 +0100

    alberi che crescono
    fabio brunori sa esattamente cosa fa quando si parla di animazioni, altro che Hermann, che nonostante le frustate non lavora come dovrebbe

--- Assets/Script/alberoInteragibile.cs
+++ Assets/Script/alberoInteragibile.cs
@@ -16,6 +16,9 @@ public class alberoInteragibile : MonoBehaviour
     public GameObject spawner;
     public Vector3 spawner_pos;
     public float distance = 1.5f;
+    public int AlberoVita;
+
+    public static bool alberoATerra;
     public enum tipo
     {
         acero,
@@ -35,25 +38,45 @@ public class alberoInteragibile : MonoBehaviour
             case tipo.pino: alb = new Pino();
                 break;
         }
+        
         Vector3 spawner_pos2 = new Vector3(Random.Range(spawner.transform.position.x + distance, spawner.transform.position.x - distance), spawner.transform.position.y , Random.Range(spawner.transform.position.z + distance, spawner.transform.position.z - distance) );
         spawner_pos = spawner_pos2;
-        
+
+        AlberoVita = alb.Vita;
+
+;
     }
     // Update is called once per frame
     void Update()
     {
         if(alb.Vita == 0)
         {
+            alberoATerra = true;
             //albero_cadente.SetActive(true);
             //Destroy(gameObject);
             gameObject.SetActive(false);
+            
             for (int i = 0; i < 3; i++)
             { 
                 Instantiate(ciocco, spawner_pos, Quaternion.identity);     
             }
 
             UI.contatore_pezzi += alb.Pezzi;
+
+            alb.Vita = AlberoVita;
+
         }
+        else
+        {
+            alberoATerra = false;
+        }
+
+        if (gameObject.activeSelf == false)
+        {
+ 
+            Debug.Log("vita albero nuovo: " + alb.Vita);
+        }
+
     }
 
     public void OnCollisionEnter(Collision other)
